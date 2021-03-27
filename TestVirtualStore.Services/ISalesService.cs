dc2146c04dc90d88b2a7c118ec533d04c2a4bc76
edagca_24 <edagca.24@gmail.com>
2021-03-27 12:21:49 -0400

    Commiting the creating sales services in dll service

---
+++ TestVirtualStore.Services/ISalesService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestVirtualStore.DataAccess;
+
+namespace TestVirtualStore.Services
+{
+    public interface ISalesService
+    {
+        IEnumerable<Sales> GetSales();
+        Sales GetSale(int id);
+        void InsertSale(Sales sale);
+    }
+}
---
+++ TestVirtualStore.Services/SalesService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestVirtualStore.DataAccess;
+
+namespace TestVirtualStore.Services
+{
+    public class SalesService
+    {
+        private IRepository<Sales> salesProfileRepository;
+
+        public UserService(IRepository<Sales> salesProfileRepository)
+        {
+            this.salesProfileRepository = salesProfileRepository;            
+        }
+
+        public IEnumerable<Sales> GetSales()
+        {
+            return salesProfileRepository.GetAll();
+        }
+
+        public Sales GetSales(int id)
+        {
+            return salesProfileRepository.Get(id);
+        }
+
+        public void InsertSales(Sales sales)
+        {
+            salesProfileRepository.Insert(sales);
+        }
+    }
+}
