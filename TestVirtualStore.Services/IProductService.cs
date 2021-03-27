12b7d45b74512b9ec7f21d0219c2787ea708d48b
edagca_24 <edagca.24@gmail.com>
2021-03-27 11:57:42 -0400

    Commiting the creating product services in dll services

---
+++ TestVirtualStore.Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestVirtualStore.DataAccess;
+
+namespace TestVirtualStore.Services
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProduct(int id);
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+    }
+}
