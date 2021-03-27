fb652570e90d3eb91e4ec6af1f194fcca3c94261
edagca_24 <edagca.24@gmail.com>
2021-03-27 11:25:56 -0400

    commiting the configuration dll repository

---
+++ TestVirtualStore.Repository/ContextApplication.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestVirtualStore.DataAccess;
+
+namespace TestVirtualStore.Repository
+{
+    public class ContextApplication : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ContextApplication> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            new ProductMap(modelBuilder.Entity<Product>());
+            new RolesMap(modelBuilder.Entity<Roles>());
+            new SalesMap(modelBuilder.Entity<Sales>());
+            new UserMap(modelBuilder.Entity<User>());
+        }
+    }
+}
